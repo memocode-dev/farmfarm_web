c7facef9af622d26e9571e29cba8f048bab633ff
miruy <dbflarla4966@naver.com>
2024-06-29 15:04:45 +0900

    Fix : 하우스 동 센서 상세정보 모달 패딩 좁게 수정

--- src/components/admin/house/section/sensor/HouseSectionSensorUpdateModal.tsx
+++ src/components/admin/house/section/sensor/HouseSectionSensorUpdateModal.tsx
@@ -1,4 +1,11 @@
-import {Dialog, DialogContent} from "@/components/ui/dialog";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle
+} from "@/components/ui/dialog";
 import {ModalContext, ModalTypes} from "@/context/ModalConext";
 import {Label} from "@/components/ui/label";
 import {useForm} from "react-hook-form";
@@ -132,107 +139,117 @@ const HouseSectionSensorUpdateModal = () => {
                 }
             }}
         >
-            <DialogContent className="rounded-lg max-w-[90%] max-h-[90vh] w-[450px] h-[700px] overflow-y-auto">
-                <Card className="w-full lg:max-w-2xl">
-                    <CardHeader>
-                        <CardTitle>하우스 동 센서 정보</CardTitle>
-                    </CardHeader>
-                    <CardContent>
-                        <form>
-                            <div className="grid w-full items-center gap-4 px-1 overflow-y-auto">
-                                {!isLoading && sensor &&
-                                    <div className="h-fit space-y-7">
-                                        <div className="flex flex-col space-y-1.5">
-                                            <Label htmlFor="id">Id</Label>
-                                            <Input disabled={true} id="id" value={sensor.id}/>
-                                        </div>
-                                        <div className="flex flex-col space-y-1.5">
-                                            <Label htmlFor="nameForAdmin">관리자용 센서명</Label>
-                                            <Input id="nameForAdmin" {...register("nameForAdmin")} />
-                                        </div>
-                                        <div className="flex flex-col space-y-1.5">
-                                            <Label htmlFor="nameForUser">사용자용 센서명</Label>
-                                            <Input id="nameForUser" {...register("nameForUser")}/>
-                                        </div>
-                                        <div className="flex flex-col space-y-1.5">
-                                            <Label htmlFor="sensorModelInfo_name">센서모델명</Label>
-                                            <Input disabled={true} id="sensorModelInfo_name"
-                                                   value={sensor.sensorModelInfo?.name}/>
-                                        </div>
-                                        <div className="flex flex-col space-y-1.5">
-                                            <Label htmlFor="sensorModelInfo_description">세선 상세</Label>
-                                            <Input disabled={true} id="sensorModelInfo_description"
-                                                   value={sensor.sensorModelInfo?.description}/>
-                                        </div>
-                                        <div className="flex flex-col space-y-1.5">
-                                            <Label htmlFor="sensorModelInfo_name">측정 타입/단위</Label>
-
-                                            {sensor.sensorModelInfo?.measurementDetails?.map((measurementDetail, index) => {
-                                                return (
-                                                    <div key={index} className="flex items-center space-x-2">
-                                                        <div>{measurementDetail.measurementType}</div>
-                                                        <Separator orientation="vertical" className="h-5"/>
-                                                        <div className="flex items-center space-x-1">
-                                                            <div>{measurementDetail.measurementUnit}</div>
-                                                            {measurementDetail.measurementUnit === "CELSIUS" &&
-                                                                <RiCelsiusLine className="w-4 h-4"/>}
-                                                            {measurementDetail.measurementUnit === "PERCENT" &&
-                                                                <RiPercentLine className="w-4 h-4"/>}
-                                                        </div>
-                                                    </div>
-                                                )
-                                            })}
-
-                                        </div>
-
-                                        <div className="flex flex-col space-y-1.5 ">
-                                            <Label htmlFor="syncStatus">센서 동기화</Label>
-                                            <Input disabled={true} id="syncStatus" value={sensor.syncStatus}/>
-                                        </div>
-                                    </div>
-                                }
-
-                                {isLoading && <div className="h-fit space-y-10">
-                                    {Array.from({length: 5}, (_, index) => (
-                                        <Skeleton key={index} className="h-[38px] w-full"/>
-                                    ))}
+            <DialogContent className="rounded-lg max-w-[90%] max-h-[90vh] w-[600px] p-0 sm:p-6">
+                <DialogHeader className="px-6 pt-6 sm:p-0">
+                    <DialogTitle>하우스 동 센서 정보</DialogTitle>
+                    <DialogDescription>
+                        하우스 동 센서의 정보를 확인하고, 수정할 수 있습니다.
+                    </DialogDescription>
+                </DialogHeader>
+                <div className="h-[620px] overflow-y-auto px-3">
+                    <form className="flex flex-col py-[10px] space-y-7">
+                        {!isLoading && sensor &&
+                            <>
+                                <div className="flex flex-col space-y-1.5">
+                                    <Label htmlFor="id">Id</Label>
+                                    <Input disabled={true} id="id" value={sensor.id}/>
                                 </div>
-                                }
-                            </div>
-                        </form>
-                    </CardContent>
-                    <CardFooter className="flex justify-between">
-                        <Button
-                            type="button"
-                            variant="destructive"
-                            onClick={() => {
+                                <div className="space-y-1">
+                                    <Label htmlFor="nameForAdmin">관리자용 센서명</Label>
+                                    <Input id="nameForAdmin" {...register("nameForAdmin")} />
+                                </div>
+                                <div className="space-y-1">
+                                    <Label htmlFor="nameForUser">사용자용 센서명</Label>
+                                    <Input id="nameForUser" {...register("nameForUser")}/>
+                                </div>
+                                <div className="space-y-1">
+                                    <Label htmlFor="sensorModelInfo_name">센서모델명</Label>
+                                    <Input disabled={true} id="sensorModelInfo_name"
+                                           value={sensor.sensorModelInfo?.name}/>
+                                </div>
+                                <div className="space-y-1">
+                                    <Label htmlFor="sensorModelInfo_description">세선 상세</Label>
+                                    <Input disabled={true} id="sensorModelInfo_description"
+                                           value={sensor.sensorModelInfo?.description}/>
+                                </div>
+                                <div className="space-y-1">
+                                    <Label htmlFor="sensorModelInfo_name">측정 타입/단위</Label>
+
+                                    <div className="flex flex-col px-1">
+                                        {sensor.sensorModelInfo?.measurementDetails?.map((measurementDetail, index) => {
+                                            return (
+                                                <div key={index} className="flex items-center space-x-2">
+                                                    <div>{measurementDetail.measurementType}</div>
+                                                    <Separator orientation="vertical" className="h-5"/>
+                                                    <div className="flex items-center space-x-1">
+                                                        <div>{measurementDetail.measurementUnit}</div>
+                                                        {measurementDetail.measurementUnit === "CELSIUS" &&
+                                                            <div className="flex items-center">
+                                                                (
+                                                                <RiCelsiusLine className="w-4 h-4"/>
+                                                                )
+                                                            </div>
+                                                        }
+                                                        {measurementDetail.measurementUnit === "PERCENT" &&
+                                                            <div className="flex items-center">
+                                                                (
+                                                                <RiPercentLine className="w-4 h-4"/>
+                                                                )
+                                                            </div>
+                                                        }
+                                                    </div>
+                                                </div>
+                                            )
+                                        })}
+                                    </div>
+                                </div>
+
+                                <div className="space-y-1">
+                                    <Label htmlFor="syncStatus">센서 동기화</Label>
+                                    <Input disabled={true} id="syncStatus" value={sensor.syncStatus}/>
+                                </div>
+                            </>
+                        }
+
+                        {isLoading && <div className="h-fit space-y-10">
+                            {Array.from({length: 5}, (_, index) => (
+                                <Skeleton key={index} className="h-[38px] w-full"/>
+                            ))}
+                        </div>
+                        }
+                    </form>
+                </div>
+                <div className="flex justify-between px-6 pb-6 sm:p-0">
+                    <Button
+                        type="button"
+                        variant="destructive"
+                        onClick={() => {
+                            toast({
+                                variant: "destructive",
+                                title: "하우스 동 센서 삭제",
+                                description: "정말로 하우스 동 센서를 삭제하시겠습니까?",
+                                action: (
+                                    <ToastAction
+                                        onClick={onDeleteSubmit}
+                                        altText="deleteHouseSectionSensor">
+                                        확인
+                                    </ToastAction>
+                                ),
+                            })
+                        }}>삭제
+                    </Button>
+
+                    <Button type="button"
+                            onClick={handleSubmit(onUpdateSubmit, () => {
                                 toast({
                                     variant: "destructive",
-                                    title: "하우스 동 센서 삭제",
-                                    description: "정말로 하우스 동 센서를 삭제하시겠습니까?",
-                                    action: (
-                                        <ToastAction
-                                            onClick={onDeleteSubmit}
-                                            altText="deleteHouseSectionSensor">
-                                            확인
-                                        </ToastAction>
-                                    ),
+                                    description: "필수 작성 필드를 확인해주세요.",
                                 })
-                            }}>삭제
-                        </Button>
-
-                        <Button type="button"
-                                onClick={handleSubmit(onUpdateSubmit, () => {
-                                    toast({
-                                        variant: "destructive",
-                                        description: "필수 작성 필드를 확인해주세요.",
-                                    })
-                                })}
-                        >
-                            수정
-                        </Button>
-                    </CardFooter>
-                </Card>
+                            })}
+                    >
+                        수정
+                    </Button>
+                </div>
             </DialogContent>
         </Dialog>
     )
